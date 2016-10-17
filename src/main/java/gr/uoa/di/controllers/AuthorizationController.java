41946599bb9f03e29988bdfbb65f1dd9a7b0c90f
Angelos Valsamis <ang.valsamis@gmail.com>
2016-10-17 18:38:16 +0300

    Complete authorisation controller actions.

--- src/main/java/gr/uoa/di/controllers/AuthorizationController.java
+++ src/main/java/gr/uoa/di/controllers/AuthorizationController.java
@@ -1,10 +1,10 @@
 package gr.uoa.di.controllers;
 
+import gr.uoa.di.entities.SimpleResponse;
 import gr.uoa.di.entities.User;
 import gr.uoa.di.repository.UserRepository;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -16,27 +16,27 @@ public class AuthorizationController {
 
 
     @ApiOperation(value = "Registers user with give username,name,surname & password", tags = "Authorization")
-    @RequestMapping(value = "/register/{username}/{name}/{surname}/{password}", method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE )
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public SimpleResponse register(@RequestBody User user) {
+        System.out.println("REGISTER " + user.toString());
 
-    public String register(@PathVariable(value="username") String username, @PathVariable(value="name") String name, @PathVariable(value="surname") String surname, @PathVariable(value="password") String password) {
-        String userId = "";
-        try {
-            User user = new User(username,name,surname, password);
-            userRepository.save(user);
-            userId = String.valueOf(user.getId());
-        }
-        catch (Exception ex) {
-            return  "Error creating the user: " + ex.toString();
-        }
-        return "User succesfully created with id = " + userId;
+            User userFromDB = userRepository.findByUsername(user.getUsername());
+            if(userFromDB==null || userFromDB.getUsername()==null)
+                user=userRepository.save(user);
+            else
+                return  new SimpleResponse("User with username "+ user.getUsername() +" already exists in database.",false);
+        return new SimpleResponse("User succesfully created with id = " + user.getId(),true);
     }
 
 
     @ApiOperation(value = "Login user with given username and password", tags = "Authorization")
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login(@RequestParam(value="username") String username, @RequestParam(value="password") String password) {
-        //check if username exists in database with given password
-        // if true send success message
-        return username +" is found in database";
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public SimpleResponse login(@RequestParam(value="username") String username, @RequestParam(value="password") String password) {
+        System.out.println("LOGIN: "+username+ " "+ password);
+        User userFromDB = userRepository.findByUsername(username);
+        if(userFromDB!=null && userFromDB.getUsername()!=null && userFromDB.getPassword().equals(password))
+            return new SimpleResponse(username +" is found in database",true);
+        else
+            return new SimpleResponse("Password is incorrect or user was not found in our database",false);
     }
 }
--- src/main/java/gr/uoa/di/entities/SimpleResponse.java
+++ src/main/java/gr/uoa/di/entities/SimpleResponse.java
@@ -6,10 +6,25 @@ package gr.uoa.di.entities;
 public class SimpleResponse {
 
 
-    private String response;
+    private String response ="";
+
+    private Boolean isOk;
 
     public SimpleResponse(String s) {
         this.response = s;
+    }
+
+    public SimpleResponse(String response, Boolean isOk) {
+        this.response = response;
+        this.isOk = isOk;
+    }
+
+    public Boolean getIsOk() {
+        return isOk;
+    }
+
+    public void setIsOk(Boolean ok) {
+        isOk = ok;
     }
 
     public String getResponse() {
